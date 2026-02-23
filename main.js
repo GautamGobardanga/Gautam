d2b8cec6283e3dd5778c58c09323ae04bc41c896
GautamGobardanga <gsgobardanga@gmail.com>
2026-02-23 23:28:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 fetch("header.html")
-.then(response=>response.text())
-.then(html=>{
-    document.getElementById ("header").innerHTML =html;
-})
-.catch(err=>console.error("Header load error:",err));
+    .then(response=>response.text())
+    .then(data=>{
+        document.getElementById ("header").innerHTML =data;
+    })
+    .catch(err=>console.error("Header load error:",err));
+function toggleDropdown(button,event) {
+    event.preventDefault(); // stop page jump
+    document.querySelectorAll (".dropdown-content.show")
+        .forEach(d=>d.classList .remove ("show"));
+    // toggle current
+    button.nextElementSibling .classList .toggle ("show");
+
+}
+
+document.addEventListener ("click",function(event) {
+    if (!event.target.classList.contains("dropbtn")) {
+        document.querySelectorAll(".dropdown-content.show")
+            .forEach(d => d.classList.remove("show"))
+    }
+});
