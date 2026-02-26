644c09806f71211815d6e23ad72a365364dbca0b
GautamGobardanga <gsgobardanga@gmail.com>
2026-02-26 21:05:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,4 @@ fetch("header.html")
         document.getElementById ("header").innerHTML =data;
     })
     .catch(err=>console.error("Header load error:",err));
-function toggleDropdown(button,event) {
-    event.preventDefault(); // stop page jump
-    document.querySelectorAll (".dropdown-content.show")
-        .forEach(d=>d.classList .remove ("show"));
-    // toggle current
-    button.nextElementSibling .classList .toggle ("show");
 
-}
-
-document.addEventListener ("click",function(event) {
-    if (!event.target.classList.contains("dropbtn")) {
-        document.querySelectorAll(".dropdown-content.show")
-            .forEach(d => d.classList.remove("show"))
-    }
-});
