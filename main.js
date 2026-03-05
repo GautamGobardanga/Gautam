445afeeae318429cefffdeb8779bc3b585ba1f82
GautamGobardanga <gsgobardanga@gmail.com>
2026-03-05 18:28:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,4 +11,29 @@ fetch("header.html")
         }
         document.getElementById(id).style.display="block";
     }
+ function tradetheory(id,file){
+        const boxes=document.getElementsByClassName("workshop-box");
+        for( let i=0; i<boxes.length; i++){
+            boxes[i].style.display="none";
+        }
+        const box= document.getElementById(id)
+              box.style.display="block"
+        if (file){
+            fetch(file)
+                 .then(response=>response.text())
+                .then(data=>box.innerText=data)
+                .catch(error => box.innerText="Unable to load file.");
+        }
+    }
+    function loadPDF(id, pdfFile) {
+    const boxes = document.getElementsByClassName("workshop-box");
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.display = "none";
+    }
+
+    const box = document.getElementById(id);
+    box.style.display = "block";
+
+    document.getElementById("pdfFrame").src = pdfFile;
+}
 
